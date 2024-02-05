a96e10767c4338a6d056739078e711bca82686ab
YusufAtti <yusuyatti@gmail.com>
2024-02-05 18:44:20 +0300

    Proje dosyası ekle.

---
+++ AlgorithmOfSha/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AlgorithmOfSha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtusename = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnsign = new System.Windows.Forms.Button();
+            this.btnlog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // txtusename
+            // 
+            this.txtusename.Location = new System.Drawing.Point(164, 66);
+            this.txtusename.Name = "txtusename";
+            this.txtusename.Size = new System.Drawing.Size(156, 27);
+            this.txtusename.TabIndex = 2;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(164, 137);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(156, 27);
+            this.txtpass.TabIndex = 3;
+            // 
+            // btnsign
+            // 
+            this.btnsign.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnsign.Location = new System.Drawing.Point(214, 201);
+            this.btnsign.Name = "btnsign";
+            this.btnsign.Size = new System.Drawing.Size(143, 50);
+            this.btnsign.TabIndex = 4;
+            this.btnsign.Text = "Sign Up";
+            this.btnsign.UseVisualStyleBackColor = false;
+            this.btnsign.Click += new System.EventHandler(this.btnsign_Click);
+            // 
+            // btnlog
+            // 
+            this.btnlog.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnlog.Location = new System.Drawing.Point(41, 201);
+            this.btnlog.Name = "btnlog";
+            this.btnlog.Size = new System.Drawing.Size(143, 50);
+            this.btnlog.TabIndex = 5;
+            this.btnlog.Text = "Log In";
+            this.btnlog.UseVisualStyleBackColor = false;
+            this.btnlog.Click += new System.EventHandler(this.btnlog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(399, 303);
+            this.Controls.Add(this.btnlog);
+            this.Controls.Add(this.btnsign);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtusename);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtusename;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnsign;
+        private System.Windows.Forms.Button btnlog;
+    }
+}
+
---
+++ AlgorithmOfSha/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgorithmOfSha
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-HP2NA5H\SQLEXPRESS;Initial Catalog=AlgoSha;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string sha256BuildCode(string s)
+        {
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+            var sb = new StringBuilder();
+            for (int i=0; i<bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+
+        }
+
+        private void btnsign_Click(object sender, EventArgs e)
+        {
+
+            if (txtusename.Text.ToString().Length == 0
+               || txtpass.Text.ToString().Length == 0)
+            {
+                MessageBox.Show(" Boxes cannot be left empty ");
+                return;
+            }
+
+            string sorgu = "SELECT KullaniciAdi FROM Kullanici WHERE KullaniciAdi = @p1";
+
+            try
+            {
+                connect.Open();
+                SqlCommand sqlCommand = new SqlCommand(sorgu, connect);
+                sqlCommand.Parameters.AddWithValue("@p1", txtusename.Text);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                bool addnewuser = false;
+
+                if (reader.HasRows)
+                { // böyle bir kullanıcı varsa vtye ekleme yapılmaz
+                    MessageBox.Show(txtusename.Text + "such a user is registered ");
+                }
+                else
+                {
+                    //veritabanına yeni kullanıcı ekleyebiliriz
+                    addnewuser = true;
+                    
+                }
+                reader.Close();
+                if (addnewuser)
+                {
+                    sqlCommand = new SqlCommand("INSERT INTO Kullanici VALUES (@p1,@p2)", connect);
+                    sqlCommand.Parameters.AddWithValue("@p1", txtusename.Text);
+                    sqlCommand.Parameters.AddWithValue("@p2", sha256BuildCode(txtpass.Text));
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error !! " + " " + ex.Message);
+            }
+
+            finally
+            {
+                if (connect != null)
+                    connect.Close();
+
+            }
+
+        }
+
+        private void btnlog_Click(object sender, EventArgs e)
+        {
+            if(txtusename.Text.ToString().Length == 0 
+               || txtpass.Text.ToString().Length == 0)
+            {
+                MessageBox.Show(" Boxes cannot be left empty ");
+                    return;
+            }
+
+            try
+            {
+                connect.Open();
+                string query = "SELECT KullaniciAdi FROM Kullanici WHERE KullaniciAdi = @p1 " +
+                    "AND Sifre = @p2";
+                SqlCommand sqlCommand = new SqlCommand(query, connect);
+                sqlCommand.Parameters.AddWithValue("@p1", txtusename.Text);
+                sqlCommand.Parameters.AddWithValue("@p2", sha256BuildCode(txtpass.Text));
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                if (reader.HasRows)
+                { // böyle bir kullanıcı varsa vtye ekleme yapılmaz
+                    MessageBox.Show("Username and Password true WELCOME " + txtusename.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password wrong ");
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Log In Error !! " + " " + ex.Message);
+            }
+
+            finally
+            {
+                if (connect != null)
+                    connect.Close();
+
+            }
+
+        }
+    }
+}
